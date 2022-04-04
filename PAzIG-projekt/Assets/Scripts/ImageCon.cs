a4e4462dec1fb3e7d8ee4389db0e768d8827b35e
Sandra <s.wieczorek199@gmail.com>
2022-04-04 14:53:07 +0200

    Answer1 correct
    x-rays added,
    correct option shown in Debug.Logs,

--- PAzIG-projekt/Assets/Scripts/ImageCon.cs
+++ PAzIG-projekt/Assets/Scripts/ImageCon.cs
@@ -20,10 +20,35 @@ public class ImageCon : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if  (GamePlay.textPointer ==1)
+        {
+            GetComponent<SpriteRenderer>().sprite = image1;
+        }
+         if  (GamePlay.textPointer ==2)
+        {
+            GetComponent<SpriteRenderer>().sprite = image2;
+        }
+         if  (GamePlay.textPointer ==3)
+        {
+            GetComponent<SpriteRenderer>().sprite = image3;
+        }
+         if  (GamePlay.textPointer ==4)
+        {
+            GetComponent<SpriteRenderer>().sprite = image4;
+        }
+         if  (GamePlay.textPointer ==5)
+        {
+            GetComponent<SpriteRenderer>().sprite = image5;
+        }
+         if  (GamePlay.textPointer ==6)
+        {
+            GetComponent<SpriteRenderer>().sprite = image6;
+        }
         
     }
     void OnMouseDown()
     {
+        GamePlay.currentSelection=gameObject.name;
         Debug.Log(gameObject.name);
     }
 }
